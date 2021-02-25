4f2c2b9541d9bdbe907a91a6af82c7c15ea7c86f
Lợii <Lii@DESKTOP-5K2RFED>
2021-02-25 22:20:54 +0700

    bai 3 lap trinh huong doi tuong (java)

---
+++ lthdtjava/src/lthdtjava/donglenh/chuong1/pheptoan.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lthdtjava.donglenh.chuong1;
+
+/**
+ *
+ * @author Lợii
+ */
+public class pheptoan {
+   public static void main(String []args){
+       //cac phep toan trong so nguyen
+//       int a=17,b=4,c;
+//       c=a+b;
+//       System.out.println("Phep cong"+a+"+"+b+"="+c);
+//       c=a-b;
+//       System.out.println("Phep tru"+b+"-"+a+"="+c);
+//       c=a*b;
+//       System.out.println("Phep nhan"+a+"*"+b+"="+c);
+//       c=a/b;
+//       System.out.println("Phep chia lay phan nguyen"+a+"/"+b+"="+c);
+//       c=a%b;
+//       System.out.println("Phep chia lay phan du"+a+"%"+b+"="+c);
+
+// cac phep toan trong so thuc
+//       double x=14.3,y=3.56,z;
+//       z=x+y;
+//       System.out.println("Phep cong"+x+"+"+y+"="+z);
+//       z=y-x;
+//       System.out.println("Phep tru"+y+"-"+x+"="+z);
+//       z=x*y;
+//       System.out.println("Phep nhan"+x+"*"+y+"="+z);
+//       z=x/y;
+//       System.out.println("Phep chia "+x+"/"+y+"="+z);
+
+
+// cac phep toan tren kieu logic
+//boolean flag1=true,flag2=false,flag3=true;
+//System.out.println("Phep AND"+(flag1&&flag2));
+//System.out.println("Phep OR"+(flag1||flag2));
+//System.out.println("Phep NOT"+(flag1));
+//System.out.println("Phep XOR"+(flag1^flag3));
+
+
+//Mot so phep toan co ban tren chuoi
+String a1="Hue University";
+String a2="university";
+System.out.println(a1.toLowerCase());
+System.out.println(a2.toLowerCase());
+System.out.println(a1.toLowerCase().contains(a2));
+a1 += " "+ a2;
+System.out.println(a1);
+
+
+   }
+}
