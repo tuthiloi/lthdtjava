4cc953ad1e8190572d0e115487c7375fbce32e33
Lợii <Lii@DESKTOP-5K2RFED>
2021-02-28 09:02:17 +0700

    bai8

---
+++ lthdtjava/src/lthdtjava/donglenh/chuong1/TaxInterface.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lthdtjava.donglenh.chuong1;
+
+/**
+ *
+ * @author Lợii
+ */
+public interface TaxInterface {
+    public double calcTax(double income);
+    
+}
